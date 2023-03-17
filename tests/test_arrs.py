17d49af48130c7831cf4af329df3996b1db476cf
Alex <santes.au@gmail.com>
2023-03-17 10:16:44 +0300

    Исправлены функции и тесты

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,9 +3,9 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
-    assert arrs.get([], 5, "test") == "test"
+    assert arrs.get([], 10, "test") == "test"
 
 
-#def test_slice():
-#    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-#    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice():
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
